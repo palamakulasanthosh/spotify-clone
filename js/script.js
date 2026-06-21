b50adc8cc52faeecb234f80dec2a1ac99715e91c
Palamakula Santhosh <santhoshsanthu24699@gmail.com>
2026-06-21 22:05:46 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -81,7 +81,7 @@ async function displayalbums(params) {
         const e = array[index];
         if (e.href.includes("songs/")) {
             let folder = e.href.split("/").splice(-1)[0]
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`)
+            let a = await fetch(`songs/${folder}/info.json`)
             let response = await a.json();
             console.log(response)
             cardcontainer.innerHTML = cardcontainer.innerHTML + `<div data-folder="${folder}" class="card">
